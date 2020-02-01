bb2a4afcf02048f3ae88746e3fd8f6c6e2c56b64
Steve Choi <stevechoi93@gmail.com>
2020-01-31 23:32:39 -0800

    add test

---
+++ README.md
@@ -0,0 +1,11 @@
+## Author
+
+- Steve Choi | https://github.com/stevechoiio
+
+## Installation Instructions
+
+Install
+`npm install`
+
+Run test
+`jest`
--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const NumberGenerator = require("./index");
-test("adds 1 + 2 to equal 3", () => {
-  const NumberGenerato = new NumberGenerator();
-  expect(NumberGenerato.multiply(1)).toBe(1 * 321);
-  expect(NumberGenerato.multiply(2)).toBe(2 * 321);
-  expect(NumberGenerato.multiply(3)).toBe(3 * 321);
-  expect(NumberGenerato.multiply(-10)).toBe(-10 * 321);
-  expect(NumberGenerato.multiply(0)).toBe(0);
+const NumberGenerator = require("./");
+test("given integers, output the value times 321", () => {
+  const calculator = new NumberGenerator();
+  expect(calculator.multiply(1)).toBe(1 * 321);
+  expect(calculator.multiply(2)).toBe(2 * 321);
+  expect(calculator.multiply(5000)).toBe(5000 * 321);
+  expect(calculator.multiply(-10)).toBe(-10 * 321);
+  expect(calculator.multiply(0)).toBe(0);
 });
